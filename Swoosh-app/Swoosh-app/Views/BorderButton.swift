58ed17e69ae5ea1687e5c277222a7d417c042b96
Eamon Allbee <eallbee@iso.com>
2018-02-17 11:45:29 -0600

    Added Border Button Class

---
+++ Swoosh-app/Swoosh-app/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  Swoosh-app
+//
+//  Created by Eamon Allbee on 2/17/18.
+//  Copyright © 2018 Allbeelean. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
