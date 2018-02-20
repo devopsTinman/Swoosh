16739f784a31d43163ac1a97103039cabc5158ce
Eamon Allbee <eallbee@iso.com>
2018-02-20 16:26:54 -0600

    Added Segues

--- Swoosh-app/Swoosh-app/ViewController.swift
+++ Swoosh-app/Swoosh-app/ViewController.swift
@@ -24,11 +24,12 @@ class ViewController: UIViewController {
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+        
+        
     }
-    
     
     
 }
